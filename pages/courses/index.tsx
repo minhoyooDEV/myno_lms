cbefbb3027f60c2d373eb88d9923f544294fcf20
myno <minho.yoo.dev@gmail.com>
2021-04-08 15:33:40 +0900

    [add] component course list

--- pages/courses/index.tsx
+++ pages/courses/index.tsx
@@ -1,13 +1,33 @@
 // courses/index.tsx
 import { NextPageContext } from 'next';
+import styled from 'styled-components';
+import { Container } from '../../components/container';
+import CourseListItem from '../../components/course/course-list-item';
 import { Course } from '../../model/course';
+import mediaquery from '../../utiles/mediaquery';
 import { CoursesMockResponse } from '../../__mock_data';
 
 interface CoursesProp {
 	data: Course[];
 }
+const Ul = styled.ul`
+	display: grid;
+	grid-template-columns: 1fr 1fr;
+	${mediaquery.md`
+		grid-template-columns: 1fr 1fr 1fr;
+`}
+`;
+
 const Courses = ({ data }: CoursesProp) => {
-	return <div>courses</div>;
+	return (
+		<Container>
+			<Ul>
+				{data.map(data => (
+					<CourseListItem key={data.id} data={data}></CourseListItem>
+				))}
+			</Ul>
+		</Container>
+	);
 };
 
 export default Courses;

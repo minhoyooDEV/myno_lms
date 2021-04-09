73fb80b12b28bd30bb4c25d0ba41b5379d8537d1
myno <minho.yoo.dev@gmail.com>
2021-04-10 03:23:35 +0900

    [add] adjust stroe

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,19 @@
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
 import { basicTheme } from '../configs/themes';
+import { StoreProvider, useInitializeStore } from '../stores';
 import '../styles/globals.css';
 
 function App({ Component, pageProps }: AppProps) {
+	// pageProps.init;
+	const store = useInitializeStore({ courseStore: { data: new Map(), list: [] } });
+
 	return (
-		<ThemeProvider theme={basicTheme}>
-			<Component {...pageProps} />
-		</ThemeProvider>
+		<StoreProvider store={store}>
+			<ThemeProvider theme={basicTheme}>
+				<Component {...pageProps} />
+			</ThemeProvider>
+		</StoreProvider>
 	);
 }
 

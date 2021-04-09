616e379cba48bf48df6f1ef837d95bfd626e7d80
myno <minho.yoo.dev@gmail.com>
2021-04-10 01:54:03 +0900

    [update] move header and main component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,12 @@
-import type { AppProps /*, AppContext */ } from 'next/app';
+import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
-import { Main } from '../components/base/main';
-import Header from '../components/header';
 import { basicTheme } from '../configs/themes';
 import '../styles/globals.css';
 
 function App({ Component, pageProps }: AppProps) {
 	return (
 		<ThemeProvider theme={basicTheme}>
-			<Header />
-			<Main>
-				<Component {...pageProps} />
-			</Main>
+			<Component {...pageProps} />
 		</ThemeProvider>
 	);
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,32 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import { Main } from '../components/base/main';
+import Header from '../components/header';
 
 export default function Home() {
 	return (
-		<Head>
-			<title>Myno LMS</title>
-			<link rel="icon" href="/favicon.ico" />
-			<ul>
-				<li>
-					<Link href="/members/sign-in">sign-in</Link>
-				</li>
-				<li>
-					<Link href="/members/register">register</Link>
-				</li>
-				<li>
-					<Link href="/courses">courses</Link>
-				</li>
-				<li>
-					<Link href="/courses/1">course id 1</Link>
-				</li>
-			</ul>
-		</Head>
+		<>
+			<Header />
+			<Main>
+				<Head>
+					<title>Myno LMS</title>
+					<link rel="icon" href="/favicon.ico" />
+					<ul>
+						<li>
+							<Link href="/members/sign-in">sign-in</Link>
+						</li>
+						<li>
+							<Link href="/members/register">register</Link>
+						</li>
+						<li>
+							<Link href="/courses">courses</Link>
+						</li>
+						<li>
+							<Link href="/courses/1">course id 1</Link>
+						</li>
+					</ul>
+				</Head>
+			</Main>
+		</>
 	);
 }

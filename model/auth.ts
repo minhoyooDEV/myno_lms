dad42f03e36b5ec3ba96c8e9564bfbce294a021b
myno <minho.yoo.dev@gmail.com>
2021-04-10 18:13:08 +0900

    [update] update auth actions (login, me, logout, etc...)

--- model/auth.ts
+++ model/auth.ts
@@ -1,18 +1,5 @@
 import { makeAutoObservable } from 'mobx';
-import { CourseContentsMockResponse } from '../__mock_data';
-
-type SignupReqestParam = {
-	email: string;
-	password: string;
-	username: string;
-	tel: string;
-};
-type LoginReqestParam = {
-	email: string;
-	password: string;
-	username?: string;
-	tel?: string;
-};
+import Router from 'next/router';
 
 interface User {
 	email: string;
@@ -20,6 +7,25 @@ interface User {
 	username: string;
 	tel: string;
 }
+
+type SignupReqestParam = {
+	email: string;
+	password: string;
+	username: string;
+	tel: string;
+};
+
+type LoginReqestParam = {
+	email: string;
+	password: string;
+	username?: string;
+	tel?: string;
+};
+
+type LoginResponseParam = {
+	user: User;
+	accessToken: string;
+};
 
 type TAuthStoreState = {
 	user: User | null;
@@ -37,52 +43,86 @@ const createAuthStore = () =>
 				body: JSON.stringify(body),
 				headers: { 'Content-Type': 'application/json' },
 			});
+
 			const data = await res.json();
+
+console.log(data)
+			if (data.errorCode) {
+				throw data;
+			}
+
+
+			this.user = data.user;
+			this.accessToken = data.accessToken;
+			localStorage.setItem('token', this.accessToken || '');
+
 			return data;
 		},
 
 		async login(body: LoginReqestParam) {
-			const res = await fetch(process.env.NEXT_PUBLIC_API_HOST + '/login', {
+			const res = await fetch('/api/login', {
 				method: 'post',
 				body: JSON.stringify(body),
 				headers: { 'Content-Type': 'application/json' },
 			});
 			const data = await res.json();
 
+			if (data.errorCode) {
+				throw data;
+			}
+
 			this.user = data.user;
 			this.accessToken = data.accessToken;
-
-			localStorage.setItem('accessToken', data.accessToken);
+			localStorage.setItem('token', this.accessToken || '');
 
 			return data;
 		},
 
 		async me() {
-			const token = localStorage.getItem('accessToken') || '';
+			const token = localStorage.getItem('token') || '';
+
+			if (!token) {
+				return;
+			}
 			const res = await fetch(process.env.NEXT_PUBLIC_API_HOST + '/me', {
 				headers: { Authorization: token },
 			});
 
 			const data = await res.json();
 
-			this.user = data.user;
-			this.accessToken = data.accessToken;
+			this.user = data;
+			this.accessToken = token;
+
 			return data;
 		},
 
 		logout() {
-			localStorage.removeItem('accessToken');
+			localStorage.removeItem('token');
+			this.user = null;
+			this.accessToken = null;
+			return true;
 		},
 
 		hydrate(hydrateData: TAuthStoreState) {
 			if (!hydrateData) return;
 
-			this.user = hydrateData.user;
-			this.accessToken = hydrateData.accessToken;
+			this.user = hydrateData?.user;
+			this.accessToken = hydrateData?.accessToken;
+		},
+
+		get isLoginned() {
+			return !!this.user;
 		},
 	});
 
 type TCreateAuthStore = ReturnType<typeof createAuthStore>;
 
 export { createAuthStore };
-export type { User, TCreateAuthStore, TAuthStoreState, SignupReqestParam, LoginReqestParam };
+export type {
+	User,
+	TCreateAuthStore,
+	TAuthStoreState,
+	SignupReqestParam,
+	LoginReqestParam,
+	LoginResponseParam,
+};

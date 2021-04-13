63bc7722ff46717fa61fa21c2d3b6aca59f8a701
myno <minho.yoo.dev@gmail.com>
2021-04-13 23:15:17 +0900

    [fix] scroll bug

--- components/base/main.tsx
+++ components/base/main.tsx
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 export const Main = styled.main`
 	position: relative;
-	overflow: hidden;
 	height: 100%;
 	width: 100%;
 	padding-top: ${({ theme }) => theme.headerHeight};
